ebc69b34dcdbd42278bdbce6bc1bb0b8994491b1
Kate Orlova <kate.orlova@unrvld.com>
2021-09-13 19:25:52 +0100

    Defined a BG property through the relevant item field

--- src/Foundation/PalettesInSitecore/Models/Palette.cs
+++ src/Foundation/PalettesInSitecore/Models/Palette.cs
@@ -1,13 +1,21 @@
 ﻿using Sitecore.Data;
 namespace PalettesInSitecore.Models
 {
-
     public class Palette
     {
         private Sitecore.Data.Items.Item _item;
 
         public virtual PaletteTheme PaletteTheme { get; set; }
-        public virtual string BG { get; set; }
+        public virtual string BG
+        {
+            get
+            {
+                if (_item != null)
+                    return _item.Fields["BG"].Value;
+                else
+                    return string.Empty;
+            }
+        }
         public virtual string CTABG { get; set; }
         public Palette(string Id)
         {
