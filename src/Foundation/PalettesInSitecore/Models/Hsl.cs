7fa954495e07a5f1c1adf244659ac3bac287b31b
Kate Orlova <kate.orlova@unrvld.com>
2021-10-04 20:48:04 +0100

    Added a class for HSL colour codes

---
+++ src/Foundation/PalettesInSitecore/Models/Hsl.cs
@@ -0,0 +1,17 @@
+﻿
+namespace PalettesInSitecore.Models
+{
+    public class Hsl
+    {
+        public Hsl(int h, int s, int l)
+        {
+            H = h;
+            S = s;
+            L = l;
+        }
+
+        public int H { get; }
+        public int S { get; }
+        public int L { get; }
+    }
+}
