6a64776b9da179759503fdf7a49f1270225bcc71
Kate Orlova <kate.orlova@unrvld.com>
2021-10-04 20:59:08 +0100

    Added a ColourConverter class from HEX to HSL codes

---
+++ src/Foundation/PalettesInSitecore/Helpers/ColourConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using PalettesInSitecore.Models;
+
+namespace PalettesInSitecore.Helpers
+{
+    public class ColourConverter
+    {
+        private const string HexPattern = @"(?i)^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$(?-i)";
+        private readonly Regex _hexRegex = new Regex(HexPattern);
+
+        private readonly Dictionary<string, Hsl> _converted = new Dictionary<string, Hsl>();
+    }
+}
