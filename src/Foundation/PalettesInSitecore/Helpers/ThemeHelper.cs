92d291191fb48ade475e05f27d15fddab0990f0b
Kate Orlova <kate.orlova@unrvld.com>
2021-10-30 23:46:12 +0100

    Created a ThemeHelper class

---
+++ src/Foundation/PalettesInSitecore/Helpers/ThemeHelper.cs
@@ -0,0 +1,29 @@
+﻿using PalettesInSitecore.Models;
+
+namespace PalettesInSitecore.Helpers
+{
+    public static class ThemeHelper
+    {
+        private static string GetPaletteKey(Palette palette)
+        {
+            var activePalette = GetActivePalette(palette);
+
+            return activePalette?.PaletteKey ?? string.Empty;
+        }
+        private static Palette GetActivePalette(Palette palette)
+        {
+            if (palette != null)
+            {
+                return palette;
+            }
+
+            return GetPagePalette();
+        }
+        private static Palette GetPagePalette()
+        {
+
+            return null;
+
+        }
+    }
+}
