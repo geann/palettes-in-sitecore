bb92dcbb9293ab6d4e1863e5b2670a551ddec687
Kate Orlova <kate.orlova@unrvld.com>
2021-11-14 13:11:54 +0000

    GetComponentTheme method

--- src/Foundation/PalettesInSitecore/Helpers/ThemeHelper.cs
+++ src/Foundation/PalettesInSitecore/Helpers/ThemeHelper.cs
@@ -4,36 +4,24 @@ namespace PalettesInSitecore.Helpers
 {
     public static class ThemeHelper
     {
+        public static string GetComponentTheme()
+        {
+            string paletteKey = GetPagePaletteKey();
+            return $"theme-{paletteKey}".ToLower();
+        }
         private static string GetPaletteKey(Palette palette)
         {
-            var activePalette = GetActivePalette(palette);
-
-            return activePalette?.PaletteKey ?? string.Empty;
-        }
-        private static Palette GetActivePalette(Palette palette)
-        {
             if (palette != null)
             {
-                return palette;
+                return palette.PaletteKey;
             }
 
-            return GetPagePalette();
+            return GetPagePaletteKey();
         }
-        private static Palette GetPagePalette()
+        private static string GetPagePaletteKey()
         {
             Sitecore.Data.Items.Item item = Sitecore.Context.Item;
-            if (item == null)
-            {
-                return null;
-            }
-
-            Palette palette = new Palette(item.Fields["Palette"].Value);
-            if (palette != null)
-            {
-                return palette;
-            }
-
-            return null;
+            return item?.Fields["Palette"].Value ?? string.Empty;
         }
     }
 }
